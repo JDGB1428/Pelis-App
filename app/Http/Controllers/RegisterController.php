833f7948f311012d25d5add524e7561999a437cd
JDGB1428 <juangalindo28@hotmail.com>
2023-05-18 10:16:08 -0500

    Añadiendo nuevas funcionalidades

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,21 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class RegisterController extends Controller
 {
+
     public function index(){
         return view('auth.register');
     }
 
     public function store(Request $request){
 
+        //Modificar el request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         $this -> validate($request,[
-            'name' => 'required|min:10',
+            'name' => 'required|max:10',
             'username' => 'required|unique:users|min:3|max:10',
             'email' => 'required|unique:users|email|max:60',
             'password' =>'required|confirmed|min:6',
         ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' =>$request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        //Redirrecionar al vista de peliculas
+        return redirect()->route('post.index');
     }
 }
